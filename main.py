9ce66ab1dda8db9d078645a3a97e5da55d1c2db0
JBragaV <jocimarcaiadobraga@gmail.com>
2022-04-03 22:03:23 -0300

    Falta inserir formatação de estilo nas células e campo assinatura

--- main.py
+++ main.py
@@ -1,39 +1,64 @@
-from io import StringIO
-
 from docx import Document
+from datetime import datetime
+
+LISTA_MESES = ["Janeiro", "Fevereiro", "Março", "Abril",
+               "Maio", "Junho", "Julho", "Agosto",
+               "Setembro", "Outubro", "Novembro", "Dezembro"]
 
 
 def iniciar(caminho_arquivo=None):
+    mes_sistema = LISTA_MESES[datetime.now().month-1]
     if caminho_arquivo is not None:
         arquivo = open(caminho_arquivo, 'rb')
         documento = Document(arquivo)
-        # documento.add_heading('Esse é o novo Heading 0 Formulário de Trocas do mês', 0)
-        # documento.add_heading('Heading 1', 1)
-        # documento.add_heading('Heading 2', 2)
-        # documento.add_heading('Heading 3', 3)
-        # documento.add_heading('Heading 4', 4)
-        # documento.add_heading('Heading 5', 5)
-        # documento.add_heading('Heading 6', 6)
-        # documento.add_heading('Heading 7', 7)
-        # documento.add_heading('Heading 8', 8)
-        # documento.add_heading('Heading 9', 9)
-        cursos = [
-            ['Felipe', 'Python', 5, 'Jocimar', 69, "18/11/1982"],
-            ['Alisson', 'Android', 4, 'Dadada', 69, "06/05/1988"],
-            ['Haynes', 'Python', 4, 'Manuzinha linda', 9, "20/06/2013"]
+        documento.add_heading(f"Trocas de {mes_sistema}", 0)
+        trocas = [
+            ['Felipe', '24/06/2024', 'A', 'Jocimar', "18/11/1982", 'D'],
+            ['Jocimar', '18/11/1982', 'B', 'Dadada', "06/05/1988", 'A'],
+            ['Haynes', '24/11/2020', 'C', 'Manuzinha linda', "20/06/2013", 'B'],
+            ['Felipe', '24/06/2024', 'A', 'Jocimar', "18/11/1982", 'D'],
             # (Instrutor, Categoria, Quantidade)
         ]
-        tabela = documento.add_table(rows=1, cols=6)
-        # Adicionamos apenas 1 linha (o titulo)
-        titulo_tabela = tabela.rows[0].cells
-        # Pegamos as células da tabela
-        titulo_tabela[0].text = "Nome do Instrutor"
-        titulo_tabela[1].text = "Categoria do Curso"
-        titulo_tabela[2].text = "Quantidade de Cursos"
-        titulo_tabela[3].text = "Nome de Aluno"
-        titulo_tabela[4].text = "Quantidade qualquer"
-        titulo_tabela[5].text = "Data"
-        for nome, categoria, quantidade, aluno, numero, data in cursos:
+        colunas_tabela = 6
+        tabela = documento.add_table(rows=1, cols=colunas_tabela)
+        # tabela.rows[0].cells[0].text =
+        # a = tabela.cell(0, 0)
+        # b = tabela.cell(0, 1)
+        # c = tabela.cell(0, 2)
+        # d = tabela.cell(0, 3)
+        # e = tabela.cell(0, 4)
+        # f = tabela.cell(0, 5)
+        # A = a.merge(b)
+        # B = A.merge(c)
+        # C = B.merge(d)
+        # D = C.merge(e)
+        # E = D.merge(f)
+        # # Fim
+        # Inicio da formatação das células do solicitante
+        linha_dados = 0
+        tabela.rows[linha_dados].cells[0].text = "Solicitante"
+        a = tabela.cell(linha_dados, 0)
+        b = tabela.cell(linha_dados, 1)
+        c = tabela.cell(linha_dados, 2)
+        A = a.merge(b)
+        B = A.merge(c)
+        # Fim
+        # Inicio da formatação das células do solicitado
+        tabela.rows[linha_dados].cells[3].text = "Solicitado"
+        d = tabela.cell(linha_dados, 3)
+        e = tabela.cell(linha_dados, 4)
+        f = tabela.cell(linha_dados, 5)
+        C = d.merge(e)
+        D = C.merge(f)
+        # Fim
+
+        # Inicio da incerção do cabeçalho dos campos necessários. indice 3
+        linha = tabela.add_row().cells
+        campos_trocas = ["Nome", "Data Serviço", "Turno", "Nome", "Data Serviço", "Turno"]
+        for indice, dados in enumerate(campos_trocas):
+            linha[indice].text = dados
+
+        for nome, categoria, quantidade, aluno, numero, data in trocas:
             linha = tabela.add_row().cells
             linha[0].text = nome
             linha[1].text = categoria
@@ -41,47 +66,31 @@ def iniciar(caminho_arquivo=None):
             linha[3].text = aluno
             linha[4].text = str(numero)
             linha[5].text = data
+        # Adicionamos apenas 1 linha (o titulo)
+        # titulo_tabela = tabela.rows[0].cells
+        # # Pegamos as células da tabela
+        # titulo_tabela[0].text = "Nome do Instrutor"
+        # titulo_tabela[1].text = "Categoria do Curso"
+        # titulo_tabela[2].text = "Quantidade de Cursos"
+        # titulo_tabela[3].text = "Nome de Aluno"
+        # titulo_tabela[4].text = "Quantidade qualquer"
+        # titulo_tabela[5].text = "Data"
+        # for nome, categoria, quantidade, aluno, numero, data in trocas:
+        #     linha = tabela.add_row().cells
+        #     linha[0].text = nome
+        #     linha[1].text = categoria
+        #     linha[2].text = str(quantidade)
+        #     linha[3].text = aluno
+        #     linha[4].text = str(numero)
+        #     linha[5].text = data
+        #
+        # print(tabela.rows[2].cells[3].text)
         documento.save(caminho_arquivo)
-    else:
-        documento = Document()
-        documento.add_heading('Heading 0 Formulário de Trocas do mês', 0)
-        documento.add_heading('Heading 1', 1)
-        documento.add_heading('Heading 2', 2)
-        documento.add_heading('Heading 3', 3)
-        documento.add_heading('Heading 4', 4)
-        documento.add_heading('Heading 5', 5)
-        documento.add_heading('Heading 6', 6)
-        documento.add_heading('Heading 7', 7)
-        documento.add_heading('Heading 8', 8)
-        documento.add_heading('Heading 9', 9)
-        cursos = [
-            ['Felipe', 'Python', 5, 'Jocimar', 69, "18/11/1982"],
-            ['Alisson', 'Android', 4, 'Dadada', 69, "06/05/1988"],
-            ['Haynes', 'Python', 4, 'Manuzinha linda', 9, "20/06/2013"]
-            # (Instrutor, Categoria, Quantidade)
-        ]
-        tabela = documento.add_table(rows=1, cols=6)
-        # Adicionamos apenas 1 linha (o titulo)
-        titulo_tabela = tabela.rows[0].cells
-        # Pegamos as células da tabela
-        titulo_tabela[0].text = "Nome do Instrutor"
-        titulo_tabela[1].text = "Categoria do Curso"
-        titulo_tabela[2].text = "Quantidade de Cursos"
-        titulo_tabela[3].text = "Nome de Aluno"
-        titulo_tabela[4].text = "Quantidade qualquer"
-        titulo_tabela[5].text = "Data"
-        for nome, categoria, quantidade, aluno, numero, data in cursos:
-            linha = tabela.add_row().cells
-            linha[0].text = nome
-            linha[1].text = categoria
-            linha[2].text = str(quantidade)
-            linha[3].text = aluno
-            linha[4].text = str(numero)
-            linha[5].text = data
-        documento.save("arquivo_novo_teste.docx")
+        return
+    print("Deu um erro aí!!!")
 
 
 if __name__ == '__main__':
-    # iniciar(r"C:\Users\jbrag\PycharmProjects\python_word\arquivo_word_teste.docx")
-    iniciar("arquivo_novo_teste.docx")
+    # iniciar("arquivo_base_trocas.docx")
+    iniciar("TROCAS.docx")
     # iniciar()
